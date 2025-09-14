fe5b96923b28575694b904ce9c25d3fb26a32375
wightkid <103604852+wightkid@users.noreply.github.com>
2025-09-14 18:52:43 -0400

    Base working input recording Base working ghost car

--- Assets/Scripts/InputRecorder.cs
+++ Assets/Scripts/InputRecorder.cs
@@ -15,6 +15,7 @@ public class InputRecorder : MonoBehaviour
     DataFrameSet frameSet = new DataFrameSet();
 
     List<DataFrame> frameDataList = new List<DataFrame>();
+    public List<DataFrame> loadedFrames = new List<DataFrame>();
 
     int timer = 0;
     public int interval = 6;
@@ -58,6 +59,17 @@ public class InputRecorder : MonoBehaviour
             SaveDataToFile();
         }
 
+        if (Input.GetKeyDown(KeyCode.RightBracket))
+        {
+            Debug.Log(loadedFrames.Count);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftBracket))
+        {
+            loadedFrames = LoadDataFromFile();
+            
+        }
+
         //Debug.Log(frameDataList.Count);
     }
 
@@ -81,16 +93,124 @@ public class InputRecorder : MonoBehaviour
     // TODO - REFACTOR FOR CUSTOM SAVE AND LOAD, JSON DOESNT SUPPORT LISTS
     private void SaveDataToFile()
     {
-        //DataFrameSet dataFrameSet = new DataFrameSet();
-        string json = JsonUtility.ToJson(frameDataList);
+        //StreamWriter streamWriter = new StreamWriter(Application.dataPath + "/frameData.txt");
+        SaveDataTestFunc("NUM", 0);
+        SaveDataTestFunc("POS", 1);
+        SaveDataTestFunc("POS", 2);
+        SaveDataTestFunc("POS", 3);
+        SaveDataTestFunc("ROT", 4);
+        SaveDataTestFunc("ROT", 5);
+        SaveDataTestFunc("ROT", 6);
+        SaveDataTestFunc("ROT", 7);
 
-        File.WriteAllText(Application.dataPath + "/framedata.txt", json);
-
+        /*
+        for (int frame = 0; frame < frameDataList.Count; frame++)
+        {
+            DataFrame tempFrame = frameDataList[frame];
+            //streamWriter.Write($"_{tempFrame.frameNumber}-{tempFrame.position}={tempFrame.rotation}");
+            //Debug.Log($"N:{tempFrame.frameNumber}, P:{tempFrame.position}, R:{tempFrame.rotation}");
+        }
+        */
     }
 
-    private void LoadDataFromFile()
+    private void SaveDataTestFunc(string mod, int caseIndex)
     {
+        StreamWriter streamWriter = new StreamWriter(Application.dataPath + $"/frameData{mod}{caseIndex}.txt");
 
+        if (caseIndex < 0 || caseIndex > 7)
+        {
+            Debug.Log("INVALID CASE INDEX, 0=FRAME NUMBER, 1=POSX, 2=POSY, 3=POSZ, 4=ROTX, 5=ROTY, 6=ROTZ, 7=ROTW");
+            return;
+        }
+
+        for (int frame = 0; frame < frameDataList.Count; frame++)
+        {
+            DataFrame tempFrame = frameDataList[frame];
+   
+            if (caseIndex == 0)
+            {
+                streamWriter.Write($"{tempFrame.frameNumber}_");
+            }
+            else if (caseIndex == 1)
+            {
+                streamWriter.Write($"{tempFrame.position.x}_");
+            }
+            else if (caseIndex == 2)
+            {
+                streamWriter.Write($"{tempFrame.position.y}_");
+            }
+            else if (caseIndex == 3)
+            {
+                streamWriter.Write($"{tempFrame.position.z}_");
+            }
+            else if (caseIndex == 4)
+            {
+                streamWriter.Write($"{tempFrame.rotation.x}_");
+            }
+            else if (caseIndex == 5)
+            {
+                streamWriter.Write($"{tempFrame.rotation.y}_");
+            }
+            else if (caseIndex == 6)
+            {
+                streamWriter.Write($"{tempFrame.rotation.z}_");
+            }
+            else if (caseIndex == 7)
+            {
+                streamWriter.Write($"{tempFrame.rotation.w}_");
+            }
+        }
+        streamWriter.Close();
+    }
+
+    private float[] LoadDataTestFunc(string mod, int caseIndex)
+    {
+        StreamReader streamReader = new StreamReader(Application.dataPath + $"/frameData{mod}{caseIndex}.txt");
+
+        string[] tempInputStringArray = streamReader.ReadToEnd().Split('_');
+        float[] tempFloatArray = new float[tempInputStringArray.Length];
+        int[] tempIntArray = new int[tempInputStringArray.Length];
+
+        for (int index = 0; index < tempFloatArray.Length; index++)
+        {
+            float currentVal;
+            string currentString = tempInputStringArray[index];
+            if (float.TryParse(currentString, out currentVal))
+            {
+                tempFloatArray[index] = currentVal;
+            }
+        }
+        streamReader.Close();
+
+        return tempFloatArray;
+    }
+
+    private List<DataFrame> LoadDataFromFile()
+    {
+        float[] dataFrameNumbers = LoadDataTestFunc("NUM", 0);
+
+        float[] dataFramePOSX = LoadDataTestFunc("POS", 1);
+        float[] dataFramePOSY = LoadDataTestFunc("POS", 2);
+        float[] dataFramePOSZ = LoadDataTestFunc("POS", 3);
+
+        float[] dataFrameROTX = LoadDataTestFunc("ROT", 4);
+        float[] dataFrameROTY = LoadDataTestFunc("ROT", 5);
+        float[] dataFrameROTZ = LoadDataTestFunc("ROT", 6);
+        float[] dataFrameROTW = LoadDataTestFunc("ROT", 7);
+
+        List<DataFrame> loadedDataFrames = new List<DataFrame>();
+
+        for (int i = 0; i < dataFrameNumbers.Length; i++)
+        {
+            DataFrame newDataFrame = new DataFrame();
+            newDataFrame.frameInterval = 6;
+            newDataFrame.frameNumber = (int)dataFrameNumbers[i];
+            newDataFrame.position = new Vector3(dataFramePOSX[i], dataFramePOSY[i], dataFramePOSZ[i]);
+            newDataFrame.rotation = new Quaternion(dataFrameROTX[i], dataFrameROTY[i], dataFrameROTZ[i], dataFrameROTW[i]);
+            loadedDataFrames.Add(newDataFrame);
+        }
+
+        return loadedDataFrames;
     }
 }
 
